87ef8b573c19833d936a2da1bf6e1338df7e320c
qwe2032133789 <qianfeng@macintosh.local>
2016-03-24 14:50:45 +0800

    跟新

---
+++ GitHubAA/Easd.h
@@ -0,0 +1,13 @@
+//
+//  Easd.h
+//  GitHubAA
+//
+//  Created by qianfeng on 16/3/24.
+//  Copyright © 2016年 xiaohua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Easd : UIView
+
+@end
---
+++ GitHubAA/Easd.m
@@ -0,0 +1,21 @@
+//
+//  Easd.m
+//  GitHubAA
+//
+//  Created by qianfeng on 16/3/24.
+//  Copyright © 2016年 xiaohua. All rights reserved.
+//
+
+#import "Easd.h"
+
+@implementation Easd
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
